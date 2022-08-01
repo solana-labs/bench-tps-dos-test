238909cf5bf53882a3c493e0ff62ca670b2d3ab3
rachaelpai <komimi.p@gmail.com>
2022-08-01 18:50:14 +0800

    auto get  cluster version and commit hash

--- start-build-solana.sh
+++ start-build-solana.sh
@@ -3,6 +3,10 @@
 set -ex
 base=$(pwd)
 ## Check ENV
+if [[ ! "$TESTNET_VER" ]];then 
+    echo No TESTNET_VER Env, use $TESTNET_VER and exit >> env.output
+    exit 1
+fi
 if [[ ! "$CHANNEL" ]];then
     CHANNEL=edge
     echo No CHANNEL Env , use $CHANNEL >> env.output
@@ -12,11 +16,10 @@ if [[ ! "$RUST_VER" ]];then
     RUST_VER=default
     echo No RUST_VER Env, use $RUST_VER >> env.output
 fi
-
 # Printout Env
 echo CHANNEL: $CHANNEL 
 echo RUST_VER: $RUST_VER
-
+echo TESTNET_VER: $TESTNET_VER
 
 ## pre-install and rust version
 sudo apt-get install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler
@@ -35,8 +38,10 @@ if [[ -d "$base/solana" ]];then
 fi
 
 git clone $repo
+cd $base/solana
+git checkout $TESTNET_VER
 # install solana in  /solana/ci
 cd $base/solana/ci
 res=$(CI_OS_NAME=linux DO_NOT_PUBLISH_TAR=true CHANNEL=$CHANNEL ./publish-tarball.sh)
-echo $res
+echo build result $res
 exit 0

4d58d8698ae4fc9f103e50a83f372dc05f53ff78
rachaelpai <komimi.p@gmail.com>
2022-08-08 19:06:47 +0800

    fix run branch error

--- exec-start-template.sh
+++ exec-start-template.sh
@@ -3,14 +3,11 @@ source ~/.bashrc
 source ~/.profile
 cd ~
 if [[ -d "bench-tps-dos-test" ]];then
-    rm  "bench-tps-dos-test"
+    rm  -rf "bench-tps-dos-test"
 fi
 if [[ -f "start-build-solana.sh" ]];then
     rm  "start-build-solana.sh"
 fi
 if [[ -f "start-dost-test.sh" ]];then
     rm  "start-dos-test.sh"
-fi
-if [[ -d "cluster-ops" ]];then
-    rm -rf "cluster-ops"
 fi
